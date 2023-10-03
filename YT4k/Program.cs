ffd719dbfccff4def6c28b8b17098a88baf11096
he bo <dooacontrol@gmail.com>
2023-10-03 19:54:55 +0800

    fun:捕获未处理的线程异常

--- YT4k/Program.cs
+++ YT4k/Program.cs
@@ -1,3 +1,4 @@
+using com.xiyuansoft.pub.log;
 using com.xiyuansoft.xyConfig;
 
 namespace YT4k
@@ -11,6 +12,10 @@ namespace YT4k
         static void Main()
         {
             FrmMain.initLog();
+
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
@@ -18,5 +23,21 @@ namespace YT4k
             FrmMain.endLog();
             xConfig.clean();
         }
+
+        #region 线程错误处理
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            FrmMain.log(LogTask.logType_erro, "应用程序错误", e.Exception);
+            MessageBox.Show("应用程序错误。请查看日志");
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            FrmMain.log(LogTask.logType_erro, "CurrentDomain错误", e.ExceptionObject as Exception);
+            MessageBox.Show("应用程序(CurrentDomain)错误。请查看日志");
+        }
+
+        #endregion
     }
 }
